36e1415f51df1f34e833d29defea461b5e9ea7ac
fpusderkisuai <federicoandres.pusderkis@alumnos.uai.edu.ar>
2022-12-18 00:25:30 -0300

    INSTALADOR add manual

--- SistemaGestorEventos.GUI/FirstTime/FirstTimeForm.Designer.cs
+++ SistemaGestorEventos.GUI/FirstTime/FirstTimeForm.Designer.cs
@@ -82,6 +82,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "Ayuda";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
--- SistemaGestorEventos.GUI/FirstTime/FirstTimeForm.cs
+++ SistemaGestorEventos.GUI/FirstTime/FirstTimeForm.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -54,6 +55,13 @@ namespace SistemaGestorEventos.GUI.FirstTime
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String ruta = Application.StartupPath.ToString();
+            BitacoraSingleton.Log(ruta + "\\Ayuda\\SGE - Manual de instalacion.pdf");
+            Process.Start(ruta + "\\Ayuda\\SGE - Manual de instalacion.pdf");
         }
     }
 }
